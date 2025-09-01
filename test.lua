aa2925fed298c02647ffe2c68b86b4e154362d42
test <lerlerlor236@gmail.com>
2025-09-01 19:54:03 +0700

    Update and rename test tp to test.lua

---
+++ test.lua
@@ -0,0 +1,21 @@
+local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+
+local Window = Library:CreateWindow{
+    Title = `test`,
+    SubTitle = "",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(830, 525),
+    Resize = true,
+    MinSize = Vector2.new(470, 380),
+    Acrylic = true,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.RightControl
+}
+
+local Tabs = {
+    Main = Window:CreateTab{
+        Title = "main",
+        Icon = "nil"
+    }
+}
+
