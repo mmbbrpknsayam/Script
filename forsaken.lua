db88972cdb9f2a237d7c711155e647f896cc2632
test <lerlerlor236@gmail.com>
2025-10-04 16:35:55 +0700

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -1,7 +1,7 @@
 local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
 
 local Window = Library:CreateWindow{
-    Title = `Glsl 4`,
+    Title = `forsaken`,
     SubTitle = "",
     TabWidth = 160,
     Size = UDim2.fromOffset(830, 525),
