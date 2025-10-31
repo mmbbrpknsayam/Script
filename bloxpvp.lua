b1e2960fce0b902d70f3f0419e91685ac39e92f9
test <lerlerlor236@gmail.com>
2025-10-31 07:42:56 +0700

    Update bloxpvp.lua

--- bloxpvp.lua
+++ bloxpvp.lua
@@ -19,21 +19,38 @@ local Tabs = {
     }
 }
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+local p = game:GetService("Players")
+local l = p.LocalPlayer
+local c = workspace:WaitForChild("Characters")
+
+local function f(s)
+	for _, model in pairs(Characters:GetChildren()) do
+		if model:FindFirstChild("HumanoidRootPart") and model.Name ~= LocalPlayer.Name then
+			model.HumanoidRootPart.Size = Vector3.new(s, s, s)
+		end
+	end
+end
 
 local Slider = Tabs.Main:CreateSlider("Slider", {
-    Title = "Hitbox",
-    Description = "",
-    Default = 1,
-    Min = 1,
-    Max = 200,
-    Rounding = 1,
-    Callback = function(Value)
-        for _, model in pairs(workspace.Characters:GetChildren()) do
-            if model:FindFirstChild("HumanoidRootPart") and model.Name ~= LocalPlayer.Name then
-                model.HumanoidRootPart.Size = Vector3.new(Value, Value, Value)
-            end
-        end
-    end
+	Title = "Hitbox",
+	Description = "",
+	Default = 1,
+	Min = 1,
+	Max = 200,
+	Rounding = 1,
+	Callback = function(x)
+		v = x
+		f(x)
+	end
 })
+
+p.PlayerAdded:Connect(function(player)
+	p.CharacterAdded:Connect(function(char)
+		repeat task.wait() until char:FindFirstChild("HumanoidRootPart")
+		if char.Name ~= l.Name then
+			char.HumanoidRootPart.Size = Vector3.new(v, v, v)
+		end
+	end)
+end)
+
+f(x)
